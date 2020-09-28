3072e7a511a885a5cf40a4c2e2c24d27be60131f
Ujjwal Bajagain <ubajagain@gmail.com>
2020-09-27 22:23:38 -0500

    adding heroku for live testing

--- PSBMS/settings.py
+++ PSBMS/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+# Needed for heroku deployment
+import django_heroku
+# define if on heroku environment
+ON_HEROKU = 'ON_HEROKU' in os.environ
+
+if ON_HEROKU:
+    django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
