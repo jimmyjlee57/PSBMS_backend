4e80696bd868f060b077a275ab1d8bc1726cf439
Ujjwal Bajagain <ubajagain@gmail.com>
2020-09-28 14:16:48 -0500

    expense crud added

--- ExpenseManagement/urls.py
+++ ExpenseManagement/urls.py
@@ -8,6 +8,8 @@ from ExpenseManagement import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('expenses/', views.expensesList, name="expensesList"),
+    path('expenses/<str:pk>', views.expenseDetails, name="expenseDetails"),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- ExpenseManagement/views.py
+++ ExpenseManagement/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import status
-
+from rest_framework import viewsets
+from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
@@ -12,12 +13,15 @@ from .models import *
 @api_view(['GET'])
 def apiOverview(request):
 	api_urls = {
-		'List All Expenses':'/expenses/'
+		'List All Expenses':'/expenses/',
+        'Expense Detail':'/expenses/<str:pk>/'
 		}
 	return Response(api_urls)
 
+
+#Expenses
 @api_view(['GET', 'POST'])
-def expensesList(request):
+def expensesList(request, format=None):
     #List all Expenses or create new
     if request.method == 'GET':
         expenses = Expenses.objects.all()
@@ -30,3 +34,27 @@ def expensesList(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def expenseDetails(request, pk, format=None):
+    #List all Expenses or create new
+    try:
+        expense = Expenses.objects.get(id = pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ExpensesSerializers(expense)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = ExpensesSerializers(expense, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        expense.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
