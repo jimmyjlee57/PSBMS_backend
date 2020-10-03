0a4e57051905280f82fc0b5477544708f0f1d2a1
AlejandroMartinez87 <alejandro.martinez3@mavs.uta.edu>
2020-10-03 17:00:36 -0500

    onHover added

--- front-end/src/Components/Sidebar.js
+++ front-end/src/Components/Sidebar.js
@@ -12,16 +12,23 @@ function Sidebar(){
 
     //const [NPC, setNPC] = useState(false);
     //const showNPC = () => setNPC(!NPC);
-
+    function increaseFont(e) {
+        //e.target.style.background = 'white';
+        e.target.style.fontSize = '34px';
+      }
+    function decreaseFont(e) {
+        //e.target.style.background = 'white';
+        e.target.style.fontSize = '28px';
+    }
     
     return (
 //if the button is clicked it changes the sidebars state
         <div className =  {sidebar ? 'sidebar-active' : 'sidebar-nonactive'}>
-            <b1 className = {sidebar ? 'mini1' : 'mini1-non'} onClick={showVisible}> Next Payment Calculation</b1>
-            <b1 className = {sidebar ? 'mini2' : 'mini2-non'}>Goals</b1>
-            <b1 className = {sidebar ? 'mini3' : 'mini3-non'}>Cashflow</b1>
-            <b1 className = {sidebar ? 'mini4' : 'mini4-non'}>Inventory</b1>
-            <b1 className = {sidebar ? 'mini5' : 'mini5-non'}>Scheduling</b1>
+            <b1 className = {sidebar ? 'mini1' : 'mini1-non'} onMouseEnter = {increaseFont} onMouseLeave = {decreaseFont} onClick={showVisible}> Next Payment Calculation</b1>
+            <b1 className = {sidebar ? 'mini2' : 'mini2-non'} onMouseEnter = {increaseFont} onMouseLeave = {decreaseFont}>Goals</b1>
+            <b1 className = {sidebar ? 'mini3' : 'mini3-non'} onMouseEnter = {increaseFont} onMouseLeave = {decreaseFont}>Cashflow</b1>
+            <b1 className = {sidebar ? 'mini4' : 'mini4-non'} onMouseEnter = {increaseFont} onMouseLeave = {decreaseFont}>Inventory</b1>
+            <b1 className = {sidebar ? 'mini5' : 'mini5-non'} onMouseEnter = {increaseFont} onMouseLeave = {decreaseFont}>Scheduling</b1>
             <button className =  {sidebar ? 'dot' : 'dot-non'} onClick={showSidebar}></button>
             {!visible ? <p/>: <Npcform/>}
         </div>
--- front-end/src/Components/npcform.js
+++ front-end/src/Components/npcform.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  './../Styles/npc.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 //https://reactjs.org/docs/forms.html
 function BasicTextFields() {
 
+  const [visible, setVisible] = useState(false);
+  const showVisible = () => setVisible(!visible);
+
   return (
-    <form className = {'form'}>
-<TextField id="outlined-search" label="State" type="search" variant="outlined" className = "space1"/>
-<TextField id="outlined-search" label="Hours" type="search" variant="outlined" className = "space2"/>
-<TextField id="outlined-search" label="$ Per Hour" type="search" variant="outlined" className = "space3"/>
-      <Button variant="outlined" color="secondary" className = "space4">
+    <form className = {!visible ? 'form' : 'form-non'}>
+    <TextField id="outlined-search" label="State" type="search" variant="outlined" className = "space1"/>
+    <TextField id="outlined-search" label="Hours" type="search" variant="outlined" className = "space2"/>
+    <TextField id="outlined-search" label="$ Per Hour" type="search" variant="outlined" className = "space3"/>
+      <Button variant="outlined" color="secondary" className = "space4" onClick={showVisible}>
         Submit
       </Button>
       <h1 className = "title">Next Payment Calculation</h1>
